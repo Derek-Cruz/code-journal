4c3c05cbfcfebed55d5a3565ad2600cc7e0e0e68
Derek Cruz <Derekcruz94@gmail.com>
2021-05-18 19:29:23 -0700

    Listened to event on photoUrl input to update src. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 /* global data */
 /* exported data */
+var $imageTag = document.querySelector('img');
+
+var $photoUrl = document.querySelector('#photo-url');
+$photoUrl.addEventListener('input', function (event) {
+  $imageTag.setAttributes('src', event.target.value);
+});
