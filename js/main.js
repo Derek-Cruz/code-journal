5f73d0a31d371ffffd9ea02db3f3fbe2873b8257
Derek Cruz <Derekcruz94@gmail.com>
2021-05-18 20:10:46 -0700

    Put forms input value into new object. #1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 /* global data */
 /* exported data */
-var $imageTag = document.querySelector('img');
-var $photoUrl = document.querySelector('.photo-url');
-var $entryForm = document.querySelector('.entry-form');
+const $imageTag = document.querySelector('img');
+const $photoUrl = document.querySelector('.photo-url');
+const $entryForm = document.querySelector('.entry-form');
 
 $photoUrl.addEventListener('input', function (event) {
   $imageTag.setAttributes('src', event.target.value);
 });
 
 $entryForm.addEventListener('submit', function (event) {
-
+  const $entries = {};
+  $entries.title = $entries.elements.title.value;
+  $entries.photourl = $entries.elements.photourl.value;
+  $entries.notes = $entries.elements.notes.value;
 });
