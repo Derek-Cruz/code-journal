b177cb80e459495740b816812cde75f681211421
Derek Cruz <Derekcruz94@gmail.com>
2021-05-18 19:38:34 -0700

    Listen for submit event on journal entry form. #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 /* global data */
 /* exported data */
 var $imageTag = document.querySelector('img');
+var $photoUrl = document.querySelector('.photo-url');
+var $entryForm = document.querySelector('.entry-form');
 
-var $photoUrl = document.querySelector('#photo-url');
 $photoUrl.addEventListener('input', function (event) {
   $imageTag.setAttributes('src', event.target.value);
+});
+
+$entryForm.addEventListener('submit', function (event) {
+
 });
